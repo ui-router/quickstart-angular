242c499557ec6937faeeaec8c849a32d20ce7955
Chris Thielen <cthielen@netflix.com>
2018-07-23 10:50:28 -0700

    travis

--- protractor.conf.js
+++ protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-      args: ["--headless", "--disable-gpu", "--window-size=800,600"]
+      args: ["--headless", "--disable-gpu", "--disable-dev-shm-usage", "--window-size=800,600"]
     }
   },
   directConnect: true,
